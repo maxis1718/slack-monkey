0e540c8a57882b88b4d91acf363e816e8fa8ef4d
Maxis Kao <maxiskao@yahoo-inc.com>
2015-06-13 01:16:55 +0800

    Add simple hello bot

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var hellobot = require('./hellobot');
+ 
+var app = express();
+var port = process.env.PORT || 3000;
+ 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+
+app.get('/', function (req, res) { res.status(200).send('Hello world!') });
+ 
+app.post('/hello', hellobot);
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+ 
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
+
