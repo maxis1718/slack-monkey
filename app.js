1330ff1f25b6b2b96cf961fa0a494ee5ca3caed3
Maxis Kao <maxiskao@yahoo-inc.com>
2015-06-17 20:00:12 +0800

    Fix body-parse missing error

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var hellobot = require('./hellobot');
 var pttBeauty = require('./pttBeauty');
- 
+
 var app = express();
 var port = process.env.PORT || 3000;
- 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
  
 app.post('/hello', hellobot);
