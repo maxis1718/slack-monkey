ca731eea7cee97594ce965fbdb036720097f5735
Maxis Kao <maxiskao@yahoo-inc.com>
2015-12-08 01:30:35 +0800

    start to promisify beauty

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var beauty = require('./lib/beauty');
+var beautyPromisify = require('./lib/beauty-promise').monkey;
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
 
 app.post('/beauty', beauty);
+app.post('/beautyPromisify', beautyPromisify);
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
